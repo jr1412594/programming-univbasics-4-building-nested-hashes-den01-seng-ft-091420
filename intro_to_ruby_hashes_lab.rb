14ff4bcf5d6da29bf67bc84ac717de75be60fd39
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-07 14:32:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :pennsuylvania_railroad => {},
+        :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shorline_railroad => {}
       }
