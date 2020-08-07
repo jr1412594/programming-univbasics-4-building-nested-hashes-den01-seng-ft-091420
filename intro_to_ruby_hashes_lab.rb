5108dd42118b05a4a1bffc46e11e7155ed2b32cd
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-07 14:23:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,8 @@ def base_hash
       :pieces => "4",
       :rent_in_dollars => {},
       :names => {},
-    }
-  },
+    },
+  }
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 
